40f7daae38b72000c4f9064e98d7ff3fa960d75f
Ibrahim Amusan <ibamusan@gmail.com>
2019-04-01 02:32:14 +0100

    change to the operator to Multiplication

--- my calculator.js
+++ my calculator.js
@@ -1,7 +1,9 @@
+// This will perform Mutiplication
+
 var A = prompt("enter first number");
 var B = prompt("enter second number");
 Number(A);
 Number(B);
-var sum = Number(A) + Number(B);
+var sum = Number(A) * Number(B);
 alert(sum);
 
