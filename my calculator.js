b3c93c769a27529deebfa06c278cdf1aac845131
Ibrahim Amusan <ibamusan@gmail.com>
2019-04-01 02:37:41 +0100

    I included some line of codes to perform Division

--- my calculator.js
+++ my calculator.js
@@ -4,6 +4,13 @@ var A = prompt("enter first number");
 var B = prompt("enter second number");
 Number(A);
 Number(B);
-var sum = Number(A) * Number(B);
-alert(sum);
+var multiply = Number(A) * Number(B);
+alert(multiply);
 
+
+var C = prompt("enter first number");
+var D = prompt("enter second number");
+Number(C);
+Number(D);
+var divide = Number(A) / Number(B);
+alert(divide);
